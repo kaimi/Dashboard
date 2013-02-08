b455abe9b94fc60ec7fcabc04aed235d1b26548c
vileda <vileda@piratenfraktion-nrw.de>
2013-02-08 21:16:20 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboard::Application.config.secret_token = 'd419ce7513cdcc9dcda214eb79e1b33a5b5291fb0e7870354f193526f8a2c2a2696564a71b730b7869ccda6d4154130e790d961f74f8f99f2adec15ff068034d'

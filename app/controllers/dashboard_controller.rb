38aecd00c65f3feeab1afac8fbd3c563d1877794
vileda <vileda@piratenfraktion-nrw.de>
2013-02-28 15:54:42 +0100

    some fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   caches_page :lists, :expires_in => 15.minutes
+  caches_page :index, :expires_in => 15.minutes
+  caches_page :wiki, :expires_in => 15.minutes
   
   def index
   end
